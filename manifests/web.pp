fca127ea76aa0ec1d5b2603b1e13bbc34b6cfe8a
mh <mh@immerda.ch>
2010-11-01 18:59:49 +0100

    add openerp-web

---
+++ manifests/web.pp
@@ -0,0 +1,19 @@
+class openerp::web {
+  require openerp::server
+
+  package{'openerp-web': ensure => present }
+
+  file{'/etc/openerp-web.cfg':
+    source => "puppet:///modules/site-openerp/${fqdn}/openerp-web.cfg",
+    require => Package['openerp-web'],
+    notifiy => Service['openerp-web'],
+    owner => root, group => openerp, mode => 0640;
+  }
+
+  service{'openerp-web':
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true,
+  }
+}
